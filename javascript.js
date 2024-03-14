9037957d81348c97d5c7d7106d966e6539932dab
jess <162760617+craicken@users.noreply.github.com>
2024-03-14 23:35:52 +0000

    check for winner after 5 games and update scores

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
 const userChoiceDiv = document.querySelector("#userChoice");
 const resultsDiv = document.querySelector("#results");
+const userScoreDiv = document.querySelector("#userScore");
+const computerScoreDiv = document.querySelector("#computerScore");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         console.log(play(button.id));
-    })
-})
+    });
+});
+
+let userScore = 0;
+let computerScore = 0;
+userScoreDiv.textContent = "Your score: " + userScore.toString();
+computerScoreDiv.textContent = "Computer score: " + computerScore.toString();
 
 
 function getComputerChoice () {
@@ -15,7 +22,6 @@ function getComputerChoice () {
 };
 
 function play (userChoice) {
-    // let userChoice = prompt("Enter rock, paper or scissors: ").toLowerCase();
     let computerChoice = getComputerChoice();
     switch (userChoice) {
         case "rock":
@@ -24,38 +30,57 @@ function play (userChoice) {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! It's a tie!";
             } else if(computerChoice === "paper") {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! You lose!";
+                computerScore++;
             } else {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! You win!";
+                userScore++;
             };
+
+            userScoreDiv.textContent = "Your score: " + userScore.toString();
+            computerScoreDiv.textContent = "Computer score: " + computerScore.toString();
+            winCheck();
             break;
+
         case "paper":
             userChoiceDiv.textContent = "You chose paper!";
             if(computerChoice === "rock") {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! You win!!";
+                userScore++;
             } else if(computerChoice === "paper") {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! It's a tie!";
             } else {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! You lose!";
+                computerScore++;
             };
+
+            userScoreDiv.textContent = "Your score: " + userScore.toString();
+            computerScoreDiv.textContent = "Computer score: " + computerScore.toString();
+            winCheck();
             break;
+
         case "scissors":
             userChoiceDiv.textContent = "You chose scissors!";
             if(computerChoice === "rock") {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! You lose!";
+                computerScore++;
             } else if(computerChoice === "paper") {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! You win!";
+                userScore++;
             } else {
                 resultsDiv.textContent = "Computer chose " + computerChoice + "! It's a tie!";
             };
+
+            userScoreDiv.textContent = "Your score: " + userScore.toString();
+            computerScoreDiv.textContent = "Computer score: " + computerScore.toString();
+            winCheck();
             break;
     }
 };
 
-function determineRoundsAndPlay () {
-    let rounds = prompt("How many rounds would you like to play? ");
-    for (let i = 0; i < rounds; i++) {
-        console.log(play());
-    };
-};
-
-// determineRoundsAndPlay();
+function winCheck() {
+    if(userScore === 5) {
+        resultsDiv.textContent = "You win the game!";
+    } else if(computerScore === 5) {
+        resultsDiv.textContent = "The computer wins!";
+    }
+}
